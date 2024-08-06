5d5e9da92eaf38a780bfe2e9335cde69d650813b
Ratu <148306263+IrvianaRatu@users.noreply.github.com>
2024-08-06 15:29:45 +0700

    Add files via upload
    TUGAS BAB 1

---
+++ tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Percobaan coba=new Percobaan();
+        coba.setVisible(true);
+    }
+    
+}
